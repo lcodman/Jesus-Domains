4fcfc60cdcf058fc263944f992a4becef442447a
lcodman <101288791+lcodman@users.noreply.github.com>
2025-06-10 14:14:48 +0200

    Update script.js

--- JesusGPT/script.js
+++ JesusGPT/script.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
         userInput.value = '';
 
         try {
-            const response = await fetch(`https://text.pollinations.ai/${encodeURIComponent(userMessage)}`);
+            const response = await fetch(`https://text.pollinations.ai/${encodeURIComponent(userMessage)}?system=You%20are%20jesus%20the%20guy%20and%20you%20are%20trying%20to%20peddle%20domains%20from%20the%20Registrar%20at%20https%3A%2F%2Fjesus-domains.comuter.at%2F%20also%20known%20as%20jesusdomains.com.%20You%20dont%20give%20qoutes%20from%20the%20bible.%20You%20are%20pro%20Lgtbq%2B`);
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
